ced54c18d8a272fb48f2bf7050c13830db6a5279
Kunal Walia <kwalia@simpliwork.com>
2026-03-18 12:12:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4203271579-182b5",
+  "appId": "1:891815764964:web:9b24f2f9d1a9d823425df1",
+  "apiKey": "AIzaSyDBLh3yvrnWiamyWWqrDtD4WJn3RhDX1ZU",
+  "authDomain": "studio-4203271579-182b5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "891815764964"
+};
